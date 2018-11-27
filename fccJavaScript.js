9a64c52615681c87f4756a9d7c31cc7f4fa44f62
efmcoder <faithefmcoder@gmail.com>
2018-11-26 20:38:41 -0500

    fccBasicJS-Strict Equality

--- fccJavaScript.js
+++ fccJavaScript.js
@@ -1,5 +1,14 @@
+//#1: Return
 function timesFive(num){
   return num * 5;
 }
-var answer = timesFive(2);
-//console.log(5);
+console.log(timesFive(5));
+
+//#2: Testing strict comparison ===
+function testStrict(val){
+  if (val === 7){
+    return "Equal";
+  }
+  return "Not Equal";
+}
+console.log(testStrict(7));
