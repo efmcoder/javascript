4e2ca83840ba4c4501fd0d1a6188f6a0434f659f
efmcoder <faithefmcoder@gmail.com>
2018-11-29 21:27:21 -0500

    Record collection - Review

--- fccJavaScript.js
+++ fccJavaScript.js
@@ -597,3 +597,72 @@ var myStorage = {
 };
 var gloveBoxContents = myStorage.car.inside["glove box"];
 console.log(gloveBoxContents);
+
+//#35: Accessing nested arrays
+console.log("#35: Accessing nested arrays");
+var myPlants = [  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+console.log(myPlants[1].list[1]); //look carefully at how pine is accessed
+
+//#36: FCC Basic JavaScript: Record Collection
+console.log("//36: FCC Basic JavaScript: Record Collection");
+
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+
+function updateRecords(id, prop, value) {
+  if (prop === "tracks" && value !== "") {
+   if(collection[id][prop]) {
+    collection[id][prop].push(value);
+   }
+   else {
+    collection[id][prop]=[value];
+   }
+  } else if (value !== "") {
+    collection[id][prop] = value;
+  } else {
+    delete collection[id][prop];
+  }
+
+  return collection;
+}
+console.log(collection);
