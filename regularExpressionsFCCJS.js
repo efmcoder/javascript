1fb7f6eecbce1e3a6fc044dd2786d43be829c1db
efmcoder <faithefmcoder@gmail.com>
2018-12-04 23:59:09 -0500

    Reg Expressions OR operator

--- regularExpressionsFCCJS.js
+++ regularExpressionsFCCJS.js
@@ -11,7 +11,18 @@ console.log("#2: Match literal strings");
 //example
 let testRegex2 = /Kevin/;
 let testStr2 = "Hello, my name is Kevin.";
-const test2 = testRegex2.test(testStr2);
+const test2 = testRegex2.test(testStr2); //TEST
 console.log(test2); //answer = true
 
 //if u were to search for kevin or KEVIN, answer would be false
+
+console.log("#4: Match a literal string with diff posibilities");
+//to match yes or no, the regex to use is /yes|no/
+// | this is the OR operator
+//search for more than 2: /yes|no|maybe/
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+console.log(result);
+
+console.log("#5: ignore case while matching");
