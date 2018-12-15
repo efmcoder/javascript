bbae94f76b0098d18c75677abf3b862f75a4be04
efmcoder <faithefmcoder@gmail.com>
2018-12-15 16:25:26 -0500

    debugging

---
+++ debuggingFCCJS.js
@@ -0,0 +1,10 @@
+console.log("#4: Use typeof");
+console.log(typeof ""); //string
+console.log(typeof 0); //number
+console.log(typeof []); //object
+console.log(typeof {}); //object
+
+let seven = 7;
+console.log(typeof seven);
+
+console.log("I\'ve had a perfect day before, this wasn\'t it.");
