d542a63877b9c1296154545479e6dca68ae435f4
efmcoder <faithefmcoder@gmail.com>
2018-12-15 23:45:37 -0500

    basic data structures

---
+++ basicDataStructures.js
@@ -0,0 +1,15 @@
+console.log("#1: Create an array");
+let myArray = ["Antonio", 34, false, "student", "Brown University"];
+console.log(myArray);
+console.log(myArray[0]);
+
+console.log("#2: Access Array using BRACKET NOTATION");
+let myVariable = myArray[0];
+console.log(myVariable);
+
+myArray[0] = "Taylor";
+console.log(myArray[0]);
+
+console.log("#3: Add items to Array using push() and unshift()");
+//push() adds elements to the end of an array
+//unshift() adds elements to the beginning
