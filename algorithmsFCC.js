0d43d1ee539ed4b967b82ffab5069dd617a2f4f2
efmcoder <faithefmcoder@gmail.com>
2019-01-06 18:04:52 -0500

    algorithms

---
+++ algorithmsFCC.js
@@ -0,0 +1,6 @@
+console.log("#1: Convert Celcius to Fahrenheit");
+function convertToFahrenheit(){
+  let Fahrenheit = (Celcius*9/5)+32;
+  return Fahrenheit
+};
+console.log(convertToFahrenheit(82));
