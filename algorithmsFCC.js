7ff9bc0d6791ed3fde03f13f35b74ef79fb96be5
efmcoder <faithefmcoder@gmail.com>
2019-01-08 23:22:25 -0500

    algorithms

--- algorithmsFCC.js
+++ algorithmsFCC.js
@@ -1,6 +1,32 @@
 console.log("#1: Convert Celcius to Fahrenheit");
-function convertToFahrenheit(){
+function convertToFahrenheit(Celcius){
   let Fahrenheit = (Celcius*9/5)+32;
   return Fahrenheit
 };
 console.log(convertToFahrenheit(82));
+
+console.log("learn how to write functions");
+
+
+function square(number){
+  return number * number;
+};
+console.log(square(30));
+
+function rectangle(num){
+  rectangle = num + num;
+  return rectangle;
+};
+console.log(rectangle(2));
+
+console.log("#2: Reverse a String");
+function reverseString(str){
+  return str.split("").reverse().join("");
+  return str;
+};
+console.log(reverseString("hello"));
+
+function splitString(name){
+  let splittString = name.split("");
+};
+console.log(splitString("hello"));
