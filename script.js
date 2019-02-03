c85832f6c14fbed9ce204556dc36356012249ab5
efmcoder <faithefmcoder@gmail.com>
2019-02-03 15:49:37 -0500

    styling box

--- script.js
+++ script.js
@@ -15,11 +15,20 @@ document.write(hours); //code to publish on website / HTML
 //console.log("Alert in JS file or in HTML file");
 //alert("Alert we've got JS onboard");
 
-var t = setInterval(move, 500);
+//INDEX: to print Hi There 3 times
+/*var arr = document.getElementsByTagName("p");
+for (var x = 0; x < arr.length; x++){
+  arr[x].innerHTML = "Hi There";
+}*/
+
+//INDEX & CSS: to move little box to the right incrementally
+//In order to work here, resolve how box.style below can read style.css file
+/*var t = setInterval(move, 500);
 var pos = 0;
 var box = document.getElementById("box");
 
 function move(){
   pos +=1;
-  box.style.left = pos + "px";
+  box.style.left = pos+"px";
 }
+*/
