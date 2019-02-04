c99551ef7163186fa274760c09b2615cf7629cbe
efmcoder <faithefmcoder@gmail.com>
2019-02-03 20:38:25 -0500

    es6

--- es6.js
+++ es6.js
@@ -1,3 +1,10 @@
 let name = "David";
-let msg = "Welcom ${name}";
+let msg = `Welcome ${name}`;
+let msg2 = "Welcome " + name;
 console.log(msg);
+console.log(msg2);
+
+let a = 8;
+let b = 2;
+let sum = `${a+b}`;
+console.log(sum);
