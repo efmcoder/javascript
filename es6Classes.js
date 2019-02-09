04bf222ba35a082ffb61811a19a064daba035aa4
efmcoder <faithefmcoder@gmail.com>
2019-02-08 23:21:41 -0500

    ES6 Inheritance

--- es6Classes.js
+++ es6Classes.js
@@ -6,7 +6,6 @@ class Rectangle {
     this.width = width;
   }
 }
-//const square = new Rectangle(5,5);
 const square = new Rectangle(5,5);
 const poster = new Rectangle(2,3);
 console.log(square.height);
@@ -26,3 +25,63 @@ const square2 = new Square(5,5)
 const poster2 = new Square(2,3);
 console.log(square2.height);
 console.log(poster2.width);
+
+class Box {
+  constructor (height, width, depth){
+    this.height = height;
+    this.width = width;
+    this.depth = depth;
+  }
+  get area(){ //area is a getter
+    return this.calcArea();
+}
+  get perimeter(){
+    return this.calcPerimeter();
+  }
+  calcArea(){ //calcArea is a method
+    return this.height * this.width;
+}
+  calcPerimeter(){
+    return 2*(this.height + this.width);
+  }
+}
+
+const amazonBox = new Box(5,5,5);
+console.log(amazonBox);
+console.log(amazonBox.area);
+console.log(amazonBox.perimeter);
+
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+  bark() {
+    console.log(this.name + " barks");
+  }
+}
+let d = new Dog("Rex");
+d.bark();
+
+//Inheritance
+//uses extends keyword to create a child of a class
+
+class Animal {
+  constuctor(name){
+    this.name = name;
+  }
+  speak(){
+    console.log(this.name + " makes a noise");
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  bark() {
+    console.log(this.name + " meows");
+  }
+}
+let c = new Cat("Meow");
+c.bark();
