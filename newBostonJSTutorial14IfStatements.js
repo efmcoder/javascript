ea3ae8fae7190594e5bd1965ea421b5ea5574b69
efmcoder <faithefmcoder@gmail.com>
2019-02-10 17:33:42 -0500

    if statements

---
+++ newBostonJSTutorial14IfStatements.js
@@ -0,0 +1,47 @@
+var apples = 44;
+var oranges = 34;
+
+if (apples >= oranges){
+  console.log(true);
+}
+
+if (2==2){
+  console.log("they r in 2")
+}
+else {
+  console.log("they are not in 2");
+
+}
+
+var fName = "suzy";
+var lName = "cody";
+
+if(fName == "suzy"){
+  if(lName == "codyy"){
+    console.log("welcome " + fName + " " + lName);
+  }
+  else{
+    console.log("you r an imposter!");
+  }
+}
+
+//testing more than 1 condition
+var first = "Margaret";
+var last = "Murphy";
+if ((first=="Margaret") && (last=="Murphy")){
+ console.log("great job Maggie!");
+}
+
+//at least 1 condition is true
+var first1 = "Margaret";
+var last1 = "Mary";
+if ((first1=="Margaret") || (last1=="Murphy")){
+ console.log("great job Maggie!");
+}
+
+
+//loops
+
+for (){
+
+}
