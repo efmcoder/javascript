7824dc585ab685554bbf8239a2bc9fdb10169f4d
efmcoder <faithefmcoder@gmail.com>
2019-03-05 21:03:05 -0500

    incremeter

---
+++ Functional Programming/incremeter.js
@@ -0,0 +1,11 @@
+var fixedValue = 4;
+
+function incremeter(){
+return fixedValue + 1;
+
+}
+
+var newValue = incremeter();
+console.log(fixedValue);
+
+//See incremeter2 for better code
---
+++ Functional Programming/incremeter2.js
@@ -0,0 +1,9 @@
+var fixedValue = 4;
+
+function incremeter(value){
+return value + 1;
+
+}
+
+var newValue = incremeter(fixedValue);
+console.log(fixedValue);
