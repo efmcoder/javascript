32fb561fd34e05aad97ebb07e96280ef98b91c17
efmcoder <faithefmcoder@gmail.com>
2019-03-06 21:22:09 -0500

    array algorithms sort etc

--- IntermediateAlgorithms/sortArrayAlphabetically.js
+++ IntermediateAlgorithms/sortArrayAlphabetically.js
@@ -14,3 +14,12 @@ var reverseSortedArray = nonSortedArray.sort(function (a, b) {
       return 0;
     });
 console.log(reverseSortedArray); // ["yo", "whatup", "lol", "hi", "bye"]
+
+
+var nonSortedArray = ['hi', 'yo', 'whatup', 'bye', 'lol'];
+var sortedArray = nonSortedArray.sort(function (a, b) {
+      if (a < b) return 1;
+      else if (a > b) return -1;
+      return 0;
+    });
+console.log(sortedArray); // ["yo", "whatup", "lol", "hi", "bye"]
