3493978fdd7dd0d66bf2a4f91b1b62529b5148a2
efmcoder <faithefmcoder@gmail.com>
2019-03-31 19:03:12 -0400

    JS programming

---
+++ codeCademy/temperature/app.js
@@ -0,0 +1,9 @@
+//change kelvin to centigrade
+
+const kelvin = 300; //the temperature we need to change.
+
+//celcius is 273 less than kelvin so create a variable for this
+
+var celcius = kelvin - 273;
+
+console.log(celcius);
