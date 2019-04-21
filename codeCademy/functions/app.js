7a1008ea80c0e49d6cb3c71d1148e8908bff7517
efmcoder <faithefmcoder@gmail.com>
2019-04-21 14:28:21 -0400

    event handlers

---
+++ codeCademy/functions/app.js
@@ -0,0 +1,6 @@
+let w = 20;
+let h = 25;
+let area = h * w;
+let perimeter = 2*( h + w);
+console.log(area);
+console.log(perimeter);
---
+++ codeCademy/raceDay/app.js
@@ -0,0 +1,13 @@
+let raceNumber = Math.floor(Math.random()*1000);
+let earlyReg = true;
+let age = 17;
+let earlyRaceTime = "9:30 am"
+if(earlyReg = true && age > 18){
+  console.log(raceNumber);
+} else {
+  console.log(1000 - raceNumber)
+};
+
+if(earlyReg = true && age > 18){
+  console.log("Your race time starts at " + earlyRaceTime + "." + " Your race number is " + raceNumber)
+};
