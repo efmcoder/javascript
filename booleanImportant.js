397c076d190ea362e25ada7943296d4b347d626e
efmcoder <faithefmcoder@gmail.com>
2019-05-01 22:38:14 -0400

    boolean, logic IMPORTANT

---
+++ booleanImportant.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+var age = prompt("How old are you?");
+
+var isAdult = (age >= 18);
+//console.log("is adult");
+
+//var isMinor = (age < 18);
+//console.log("is not adult");
+
+console.log("User is an adult: " + isAdult);
+
+var Ten = (age == 10);
--- promptUserInput.js
+++ promptUserInput.js
@@ -2,3 +2,5 @@
 var favoriteMovie = prompt("What is your favorite movie?");
 var response = " is a great movie!!!";
 console.log(favoriteMovie + response);
+
+
