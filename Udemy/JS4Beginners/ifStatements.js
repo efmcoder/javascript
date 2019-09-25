256f2e366e1a9e56db309a69abc9800de0c21e74
efmcoder <faithefmcoder@gmail.com>
2019-09-24 22:32:46 -0400

    if Statements in html and JavaScript

---
+++ Udemy/JS4Beginners/ifStatements.js
@@ -0,0 +1,10 @@
+var MikeAge = 30;
+var JohnAge = 30;
+if(MikeAge === JohnAge){
+  console.log("Mike is the same age as John")
+}else if(MikeAge > JohnAge){
+    console.log("Mike is older than John")
+  }
+  else if(MikeAge < JohnAge){
+      console.log("Mike is younger than John")
+    }
