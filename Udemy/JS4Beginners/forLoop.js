8c88968eb474c90cbfa2103f0f074a6e1814f3a8
efmcoder <faithefmcoder@gmail.com>
2019-09-25 22:47:49 -0400

    loops

---
+++ Udemy/JS4Beginners/forLoop.js
@@ -0,0 +1,11 @@
+var i = 0;
+
+for(i = 0; i < 5; i++){
+  console.log(i);
+}
+console.log("\n");
+
+
+for(i = 10; i >= 0; i--){
+  console.log(i);
+}
---
+++ Udemy/JS4Beginners/pyramidLoop.js
@@ -0,0 +1,10 @@
+var i;
+console.log("<center>");
+for(i = 0; i <=5; i++ ){
+  var s;
+    for(s = 0; s <= i; s++){
+      console.log("s");
+  }
+  console.log("\n");
+}
+console.log("</center>");
