f5e410017a6569dca77a889032bcce00a8f4b8b6
efmcoder <faithefmcoder@gmail.com>
2019-09-25 21:17:07 -0400

    loops, while, do while line breaks

---
+++ Udemy/JS4Beginners/whileLoop.js
@@ -0,0 +1,20 @@
+var num = 0;
+while(num < 5){
+  console.log(num);
+  num++;
+}
+console.log("\n");
+
+var count = 10;
+while(count >= 0){ //equal sign should be after the > sign or else...forever loop!
+  console.log(count);
+  count--;
+}
+
+console.log("\n");
+//DO WHILE LOOP EXECUTES THE CODE FIRST
+var i = 0;
+do{
+  console.log(i);
+  i++;
+} while(i<7);
