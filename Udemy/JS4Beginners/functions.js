833cc636263e436c25a97de3cca068027f76b91b
efmcoder <faithefmcoder@gmail.com>
2019-09-26 23:32:13 -0400

    math and more loops

---
+++ Udemy/JS4Beginners/functions.js
@@ -0,0 +1,16 @@
+function name(){
+
+}
+
+//FUNCTION INPUT
+//prompt user for number1
+//prompt user for number 2
+
+//FUNCTION PROCESS
+//set prompt1 to equal number1
+//set prompt2 to equal number2
+//define variable Calculation
+//set calculation=number1+number2
+
+//FUNCTION OUPUT
+//output calculation
