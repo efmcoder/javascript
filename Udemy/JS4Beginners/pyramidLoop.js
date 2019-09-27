50f1a1d45986e73db2edb8ea2e98632aa4f56958
efmcoder <faithefmcoder@gmail.com>
2019-09-26 21:08:13 -0400

    how to create a Pyramid

--- Udemy/JS4Beginners/pyramidLoop.js
+++ Udemy/JS4Beginners/pyramidLoop.js
@@ -1,10 +1,29 @@
-var i;
-console.log("<center>");
-for(i = 0; i <=5; i++ ){
-  var s;
-    for(s = 0; s <= i; s++){
-      console.log("s");
+function createEgpytPyramid(steps)
+{
+for(var i = 0; i < steps; i++ ){
+var height = " ";//will read what's in your function parameter and set that as the height of the pyramid
+  for(var j = 0; j<steps-i; j++){
+      height+=" "; //creates the left slope
   }
-  console.log("\n");
+  for(var k = 0; k<=i; k++){
+    height += "x ";//creates the right slope
+  }
+    console.log(height);
 }
-console.log("</center>");
+}
+createEgpytPyramid("5")
+
+function createPyramid(rows)
+{
+for(var i=0;i<rows;i++) {
+var output=" ";
+    for(var j=0;j<rows-i;j++) {
+        output+=" ";//creates the left slope
+    }
+    for(var k=0;k<=i;k++) {
+       output += "x ";//creates the right slope
+    }
+    console.log(output);
+}
+}
+createPyramid('5')
