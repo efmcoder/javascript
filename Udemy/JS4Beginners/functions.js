b0bd2b9b06fc7b232eddf3bb5a6c66df584cd84c
efmcoder <faithefmcoder@gmail.com>
2019-09-27 22:21:36 -0400

    functions in JavaScript

--- Udemy/JS4Beginners/functions.js
+++ Udemy/JS4Beginners/functions.js
@@ -1,6 +1,14 @@
-function name(){
-
+function addNumbers(num1, num2){
+  var calculation = num1 + num2;
+  console.log(calculation);
 }
+addNumbers(7, 4);
+
+function krogerSales(store1, store2){
+  var calculation = store1 + store2;
+  console.log(calculation);
+}
+krogerSales(20000, 50000);
 
 //FUNCTION INPUT
 //prompt user for number1
