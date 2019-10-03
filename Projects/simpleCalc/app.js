fcfe7aa63774eee16b764d9ad3807eaa933f93db
efmcoder <faithefmcoder@gmail.com>
2019-10-03 00:41:41 -0400

    Event Handlers and Functions combining

---
+++ Projects/simpleCalc/app.js
@@ -0,0 +1,14 @@
+function addXandY(){
+  var num1 = 10;
+  var num2 = 50;
+  var calculation = num1 + num2;
+  console.log(calculation);
+}
+addXandY();
+
+
+function addNumbers(num1, num2){
+  var calculation = num1 + num2;
+  console.log(calculation);
+}
+addNumbers(100, 100);
