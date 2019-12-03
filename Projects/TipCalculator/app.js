d933fc94a876f85a3a6e84a068fe49af74b13d0b
efmcoder <faithefmcoder@gmail.com>
2019-12-03 00:26:24 -0500

    Calculator

--- Projects/TipCalculator/app.js
+++ Projects/TipCalculator/app.js
@@ -1,3 +1,27 @@
+//WORKING FUNCTION IS:
+var calc = function(){
+var bill = Number(document.getElementById("bill").value);
+var tip = bill * 0.15;
+
+/* WOULD PREFER TO USE A FUNCTION
+var tip = function{
+  if(bill < 30) {
+    tip  = 0.2;
+  } else if (bill >=30 && bill < 200) {
+    tip = 0.15;
+  } else {
+    tip = 0.1;
+  }
+  return tip;
+}*/
+
+
+var totalBill = tip + bill;
+
+document.getElementById("tip").innerHTML = "$ " + Number(tip);
+document.getElementById("totalBill").innerHTML = "$ " + Number(totalBill);
+}
+
 /*var bill;
 var tip = [0.2, 0.15, 0.1];
 
@@ -15,9 +39,12 @@ function calculateTip(bill){
 }
 console.log(calculateTip(75));*/
 
-/*var calc = function(){
+
+//FUNCTION BELOW NOT WORKING
+/*
+var calc = function(){
   var bill = Number(document.getElementById('bill').value);
-  /*var tip = function{
+  var tip = function{
     if(bill < 30) {
       tip = 0.2;
     } else if (bill >=30 && bill < 200) {
@@ -25,11 +52,14 @@ console.log(calculateTip(75));*/
     } else {
       tip = 0.1;
     }
-    return tip * bill;
-}*/
+    return tip;
+}
 
-//var tip =
-  function calculateTip(bill){
+
+//NOT WORKING BELOW
+/*
+var tip =
+function calculateTip(bill){
     if(bill < 30){
       tip = 0.2;
     } else if (bill >=30 && bill <200) {
@@ -37,20 +67,19 @@ console.log(calculateTip(75));*/
     } else {
       tip = 0.1;
     }
-    return tip;
-    return tip + bill;
-    //return totalBill = (tip * bill);
-
-console.log(calculateTip(25));
+    return tip*bill;
+    return bill + (tip*bill);
+}
+console.log("The tip is " + "$" + calculateTip(25));
 console.log(calculateTip(80));
 console.log(calculateTip(500));
 
-//var totalBill = bill + tip;
+var totalBill = bill + tip;
+*/
+  /*document.getElementById("tip").innerHTML = "$" + Number(tip);
+  document.getElementById("total").innerHTML = "$" + Number(totalBill);*/
 
-  //document.getElementById("tip").innerHTML = "$" + Number(tip);
-  //document.getElementById("total").innerHTML = "$" + Number(totalBill);
-
-//console.log(tip);
+//console.log("the tip also is " + "$" + tip);
 //console.log(totalBill);
 
-}
+//}
