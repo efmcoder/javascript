beed640ea5304e765bf0e35205186633eea28de4
efmcoder <faithefmcoder@gmail.com>
2019-12-10 00:06:30 -0500

    inputs buttons and calculate

--- Projects/Experiment/app.js
+++ Projects/Experiment/app.js
@@ -1,9 +1,14 @@
-
-//var bill = 10;
 var calc = function(){
 var bill = Number(document.getElementById("bill").value);
 var tip = bill * 0.15;
 
 //this is playing on the element with id "tip" in index.html (the document)
-document.getElementById("tip").innerHTML = "$ " + Number(tip);
+document.getElementById("tip").innerHTML = " $" + Number(tip);
+
+
+var income = Number(document.getElementById("income").value);
+var tax = income * 0.4;
+
+document.getElementById("tax").innerHTML = "$"+Number(tax);
+
 }
