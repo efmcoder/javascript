ebead72cad821560bf0b15a2aca8a15a95098d51
efmcoder <faithefmcoder@gmail.com>
2020-03-25 00:12:33 -0400

    For Loop

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+var i;
+for(i = 0; i <=10; i++){
+	console.log(i);
+}
+
+console.log("\n");
+
+var i;
+for(i = 10; i>0; i--){
+	console.log(i);
+}
---
+++ split.js
@@ -0,0 +1,24 @@
+var Bryant = new Array();
+Bryant["color"] = "blue";
+Bryant["food"] = "pizza";
+
+var LeBron = new Array();
+LeBron["color"] = "green";
+LeBron["food"] = "cheese";
+
+console.log("Bryant's favorite food is " + Bryant["food"] + " while LeBron's is " + LeBron["food"]);
+
+var dog = "i can bark at a puppy";
+console.log(dog.length);
+var splitString = dog.split(" ");
+var splitString2 = dog.split("");
+console.log(splitString);
+console.log(splitString2);
+var str = dog.split(" ").reverse().join(" ");
+var str2 = dog.split("").reverse().join("");
+var str3 = dog.split("").reverse().join(" ");
+var str4 = dog.split(" ").reverse().join("");
+console.log(str);
+console.log(str2);
+console.log(str3);
+console.log(str4);
