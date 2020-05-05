150aff4e7964fc04c3678ffbc5abf5c9701884a4
efmcoder <faithefmcoder@gmail.com>
2020-05-04 21:54:57 -0400

    Calculator in JavaScript

---
+++ DOMManipulation/app.js
@@ -0,0 +1,10 @@
+var myHeading = document.querySelector("#title");
+var buttonToBeClickedOn = document.querySelector("#button");
+
+
+	buttonToBeClickedOn.addEventListener("click", function(){
+	myHeading.toggle("changeOnClick");
+
+})
+
+
